646f90217c7a5f7dd0bd1753be556c976293c386
jberli <justinberli@gmail.com>
2024-03-17 19:59:56 +0100

    started lookup input div

--- explorer/static/explorer/js/app/initialization.js
+++ explorer/static/explorer/js/app/initialization.js
@@ -4,6 +4,9 @@
  */
 
 $(document).ready(function() {
+    let header = make_div(id='header');
+    document.body.appendChild(header);
+
     let container = make_div(id='container')
     let mapdiv = make_div(id='map')
     container.appendChild(mapdiv)
@@ -11,8 +14,10 @@ $(document).ready(function() {
 
     initialize_map(mapdiv);
 
-    let lookup = make_div(id='lookup')
-    container.appendChild(lookup);
+    let lookupcontainer = make_div(id='lookup-container');
+    let lookup = make_input(id='lookup');
+    lookupcontainer.appendChild(lookup);
+    header.appendChild(lookupcontainer);
 
     ajax({ bonjour: '152' }, 'lookup/', 'POST', function(e) {
         console.log(e)
--- explorer/static/explorer/js/app/interface/dom.js
+++ explorer/static/explorer/js/app/interface/dom.js
@@ -3,10 +3,20 @@
  * DOM related functions.
  */
 
-function make_div(id=null, c=null, html=null) {
+function make_div(id=null, c=null, html=null, parent=null) {
     let div = document.createElement('div');
-    if (id !== null) { div.setAttribute('id', id) }
-    if (c !== null) { div.setAttribute('class', c) }
-    if (html !== null) { div.innerHTML = html }
-    return div
+    if (id !== null) { div.setAttribute('id', id); }
+    if (c !== null) { div.setAttribute('class', c); }
+    if (html !== null) { div.innerHTML = html; }
+    if (parent !== null) { parent.appendChild(div); }
+    return div;
+}
+
+function make_input(id=null, c=null, placeholder=null, spellcheck=false) {
+    let input = make_div(id=id, c=c)
+    input.setAttribute('contenteditable', 'true');
+    if (placeholder !== null) { input.setAttribute('placeholder', placeholder); }
+    if (spellcheck) { input.setAttribute('spellcheck', 'true'); }
+    else { input.setAttribute('spellcheck', 'false'); }
+    return input
 }
