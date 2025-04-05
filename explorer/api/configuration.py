d1925a5d36cfd63fed07faaed013be39ea91b0e6
jberli <justinberli@gmail.com>
2025-04-05 16:28:30 +0200

    added flags in header

--- explorer/api/configuration.py
+++ explorer/api/configuration.py
@@ -1,8 +1,30 @@
 import yaml
+import csv
 
 def get_configuration():
     """
     Retrieve the configuration along with taxonomic information on the showcased taxon.
     """
     c = yaml.load(open('explorer/static/explorer/conf/configuration.yaml', 'r'), Loader=yaml.FullLoader)
+
+    r = open('.update/history', 'r')
+    updates = list(csv.reader(r, delimiter='\t'))
+
+    update = None
+    initialization = None
+    for u in reversed(updates):
+        if u[4] == 'SUCCESS':
+            update = u[2]
+            break
+
+    for u in reversed(updates):
+        if u[0] == 'initialization' and u[4] == 'SUCCESS':
+            initialization = u[2]
+            break
+    
+    c['database'] = {
+        'update': update,
+        'initialization': initialization
+    }
+
     return c
--- explorer/static/explorer/js/app/interface/header.js
+++ explorer/static/explorer/js/app/interface/header.js
@@ -3,7 +3,7 @@
  * Define the header of the application.
  */
 
-import { addSVG, makeDiv } from "../generic/dom.js";
+import { addClass, addSVG, makeDiv } from "../generic/dom.js";
 import Widget from "./widget.js";
 
 class Header extends Widget {
@@ -12,27 +12,34 @@ class Header extends Widget {
         this.container = makeDiv('header', 'sub-panel');
         this.parent.append(this.container);
 
-        this.logo = new Logo(this);
-        // this.search = new Search(this);
-        // this.information = new Information(this);
+        this.logo = makeDiv(null, 'header-logo');
+        addSVG(this.logo, new URL('/static/explorer/img/logo.svg', import.meta.url));
+        this.container.append(this.logo);
+
+        this.foldable = makeDiv(null, 'header-foldable');
+        this.container.append(this.foldable);
+
+        let language = this.params.languages.current;
+        let lastupdate = new Date(this.params.database.update);
+        let lastinit = new Date(this.params.database.initialization);
+        let locale = this.params.languages.available[language].locale;
+
+        let options = { weekday: "short", year: "numeric", month: "long", day: "numeric" };
+        this.updatecontainer = makeDiv(null, 'header-update-container');
+        this.update = makeDiv(null, 'header-update', this.params.texts.database.update[language] + ' ' + lastupdate.toLocaleDateString(locale, options));
+        this.init = makeDiv(null, 'header-update', this.params.texts.database.initialization[language] + ' ' + lastinit.toLocaleDateString(locale, options));
+        this.updatecontainer.append(this.update, this.init);
+
+        this.languages = makeDiv(null, 'header-languages-container');
+        for (let [lang, value] of Object.entries(this.params.languages.available)) {
+            let l = makeDiv(null, 'header-language');
+            addSVG(l, new URL('/static/explorer/img/languages/' + lang + '.svg', import.meta.url));
+            if (lang === language) { addClass(l, 'active') }
+            this.languages.append(l);
+        }
+
+        this.foldable.append(this.updatecontainer, this.languages);
     }
 }
-
-class Logo {
-    constructor(header) {
-        this.header = header;
-        this.container = makeDiv('header-logo', 'header-module');
-        addSVG(this.container, new URL('/static/explorer/img/logo.svg', import.meta.url));
-        this.header.container.append(this.container);
-    }
-}
-
-// class Information {
-//     constructor(header) {
-//         this.header = header;
-//         this.container = makeDiv('header-information', 'header-module');
-//         this.header.container.append(this.container);
-//     }
-// }
 
 export default Header;
